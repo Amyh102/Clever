46b8ae4f9e03a28d5257d803be5a81d685eeb305
Lucy Han <lucyhan246@gmail.com>
2016-10-22 21:42:51 -0700

    added credit card recommendation

--- synchrony.py
+++ synchrony.py
@@ -5,8 +5,28 @@ header = {"Content-Type": "application/json",
           "username": "giveitatry",
           "password": "Sh0wT!me"}
 
+categories = {"Pets":"PetSmart Credit Card",
+             "Grocery":"Whole Foods Credit Card",
+             "Automotive":"BP Credit Card",
+             "Electronic":"Best Buy Credit Card",
+             "Gasoline":"Shell Credit Card",
+             "Outdoor Living":"Bass Pro Shops Credit Card",
+             "Jewelry":"Pandora Credit Card",
+             "Bill Pay":" Credit Card",
+             "Restaurants":"Olive GardenCredit Card",
+             "Travel":"United Airlines Credit Card",
+             "Clothing":"American Eagle Credit Card",
+             "Baby":"Baby's R Us Credit Card",
+             "Utilities":"Nest Credit Card",
+             "Entertainment":"Cineplex Credit Card",
+             "Books":"Chapters Credit Card",
+             "Makeup":"Sephora Credit Card",
+             "Sports":"Sport Chek Credit Card",
+             "Insurance":"Local Insurance Provider Credit Card",
+             }
 
-def next_card(account_num):
+
+def next_purchase(account_num):
     data = {"accountNum": account_num}
     req = requests.post("https://syf2020.syfwebservices.com/syf/nextMostLikelyPurchase", headers=header, data=json.dumps(data))
     next_purchase = req.json()
@@ -17,6 +37,8 @@ def next_card(account_num):
 
     return most_probable['categoryName']
 
+def next_card(category):
+    return categories[category]
 
 if __name__ == '__main__':
     account_num = raw_input("Please enter your account number: ")
@@ -25,4 +47,8 @@ if __name__ == '__main__':
     while not 99999 < account_num < 105000:
         account_num = int(raw_input('Please enter your full account number:'))
 
-    print "You may be interested in our {} cards".format(next_card(account_num))
+
+    category = next_purchase(account_num)
+    card = next_card(category)
+    print "You may be interested in our {} cards".format(category)
+    print "Consider applying for our",card, "cards"
